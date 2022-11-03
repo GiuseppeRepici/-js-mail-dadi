79c204480e79372320c0806bd9147f54db55e333
GiuseppeRepici <giuseppe.repici95@gmail.com>
2022-11-03 16:52:48 +0100

    gioco  dati fatto senza array

---
+++ js/dadi.js
@@ -0,0 +1,16 @@
+let dado1 = Math.floor((Math.random() * (7-1)) +1);
+let dado2 = Math.floor((Math.random() * (7-1)) +1);
+console.log(dado1);
+console.log(dado2);
+
+const dadoUtente = document.getElementById("dadoutente").innerHTML= "Dado utente: " + dado1;
+const dadoCpu = document.getElementById("dadocpu").innerHTML= "Dado cpu: " + dado2;
+
+console.log(dadoUtente);
+console.log(dadoCpu);
+if (dado1 == dado2){
+    const vincente = document.getElementById("vincitore").innerHTML= "stesso risultato , parità " ;
+} else if (dado1 > dado2){
+    const vincente = document.getElementById("vincitore").innerHTML= "vince l'utente" ;
+} else vincente = document.getElementById("vincitore").innerHTML= "vince la cpu " ;
+

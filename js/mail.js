2bc0ed0447b4eb1dd7a06b95d257f4bd515a1ed3
GiuseppeRepici <giuseppe.repici95@gmail.com>
2022-11-03 17:18:41 +0100

    completed

--- js/mail.js
+++ js/mail.js
@@ -1,23 +1,25 @@
 const bottonecarica = document.getElementById("btn1");
-let x;
-let z = 0;
+
 const listaEmail = ["mail@mail.com" ,"mail@gmail.com" , "mail1@gmail.com"]
 
 bottonecarica.addEventListener("click" , 
     function (){
+
+        trovata = false;
+
         const emailUtente = document.getElementById("inputmail").value;
         console.log(emailUtente);
         for(let i=0 ; i < listaEmail.length ; i++) {
-            x = listaEmail[i];
-            console.log(x);
-            if ( x === emailUtente ){
-                z = 1;
+            if ( listaEmail[i] === emailUtente ){
+                trovata = true;
             } 
-            console.log(z);
+            console.log(trovata);
         }
-        if (z == 1){
-            const stampoavideo = document.getElementById("rispostajs").innerHTML= "presente" ;
-        } else stampoavideo = document.getElementById("rispostajs").innerHTML= "non è presente" ;
+        if (trovata == true){
+            document.getElementById("rispostajs").innerHTML= "presente" ;
+        } else {
+            document.getElementById("rispostajs").innerHTML= "non è presente" ;
+        }
     }
 )
 
